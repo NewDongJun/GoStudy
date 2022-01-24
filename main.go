794aef962182a5a5fdee3d038204f95d704070a8
신동준 <shindj4613@outlook.com>
2022-01-24 12:21:27 +0900

    for, range

--- main.go
+++ main.go
@@ -22,15 +22,35 @@ func lenAndUpper2(name string) (length int, uppercase string) {
 
 func repeatMe(words ...string) {
 	fmt.Println(words)
-
 }
+
+func superAdd(numbers ...int) int {
+	// for index, number := range numbers {
+	// 	fmt.Println(index, number)
+	// }
+
+	// for i := 0; i < len(numbers); i++ {
+	// 	fmt.Println(numbers[i])
+	// }
+
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
 func main() {
-	fmt.Println(multiply(2, 2))
-	totallength, _ := lenAndUpper("nico")
-	fmt.Println(totallength)
+	// fmt.Println(multiply(2, 2))
+	// totallength, _ := lenAndUpper("nico")
+	// fmt.Println(totallength)
 
-	a, b := lenAndUpper2("coco")
-	fmt.Println(a, b)
+	// a, b := lenAndUpper2("coco")
+	// fmt.Println(a, b)
 
-	repeatMe("nico", "lynn", "ann")
+	// repeatMe("nico", "lynn", "ann")
+
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
+
 }
