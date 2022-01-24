0703aa6f48110488dbb0134f5f4ced9cce53aacf
신동준 <shindj4613@outlook.com>
2022-01-24 11:56:27 +0900

    func part 2

--- main.go
+++ main.go
@@ -13,6 +13,13 @@ func lenAndUpper(name string) (int, string) {
 	return len(name), strings.ToUpper(name)
 }
 
+func lenAndUpper2(name string) (length int, uppercase string) {
+	defer fmt.Println("im done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
+
 func repeatMe(words ...string) {
 	fmt.Println(words)
 
@@ -21,6 +28,9 @@ func main() {
 	fmt.Println(multiply(2, 2))
 	totallength, _ := lenAndUpper("nico")
 	fmt.Println(totallength)
+
+	a, b := lenAndUpper2("coco")
+	fmt.Println(a, b)
 
 	repeatMe("nico", "lynn", "ann")
 }
